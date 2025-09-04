9c4df08ae4f8fdd9f1baa25c62a27614478c4cab
Doug Park <parkdn@gmail.com>
2025-09-04 14:18:12 -0500

    Add script to rename files by removing unique identifiers from filenames

---
+++ remove_unique_code.py
@@ -0,0 +1,16 @@
+import os
+import re
+
+root_dir = "ebooks/Comics"
+pattern = re.compile(r"^(.*)_([A-Z]{5})$")
+
+for folder, _, files in os.walk(root_dir):
+    for filename in files:
+        name, ext = os.path.splitext(filename)
+        match = pattern.match(name)
+        if match:
+            new_name = match.group(1) + ext
+            old_path = os.path.join(folder, filename)
+            new_path = os.path.join(folder, new_name)
+            # os.rename(old_path, new_path)
+            print(f"Renamed: {old_path} -> {new_path}")
